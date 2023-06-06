b9b40945a788000550bdf29eb93a3ef9af4474e0
Deepak Rana <hypermac@Deepaks-MacBook-Pro.local>
2023-06-06 22:07:31 +0530

    setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://blogadmin:m0s7aeE72mosmjLr@seoblog.g2gqtux.mongodb.net/"
+  )
+  .catch((err) => console.log("errrr", err));
